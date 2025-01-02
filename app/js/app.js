7e9984a6c0aa9ca10adfc001e188003dcac684eb
vmnekrasovv <vmnekrasov777@gmail.com>
2025-01-02 21:24:47 +0300

    header navbar (adaptive)

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,20 @@
 	$(document).ready(function(){ 
 		
 	});
+
+	$(window).on('load resize', function(){
+	
+		let window_width = window.innerWidth;
+
+		let navbar = $('.navbar .container');
+		let navbarDrop = $('.navbar-dropdown');
+		let menu = $('.header-menu');
+		let toolbar = $('.toolbar');
+
+		if(window_width <= 1400){
+			menu.add(toolbar).appendTo(navbarDrop);
+		} else {
+			menu.add(toolbar).appendTo(navbar);
+		}
+	});
 })(jQuery);
