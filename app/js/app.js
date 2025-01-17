582a65ed351291899b013a59a566d4aa75c26d4b
vmnekrasovv <vmnekrasov777@gmail.com>
2025-01-17 10:25:21 +0300

    header adaptive fix (in progress)

--- app/js/app.js
+++ app/js/app.js
@@ -10,12 +10,43 @@
 		let navbar = $('.navbar .container');
 		let navbarDrop = $('.navbar-dropdown');
 		let menu = $('.header-menu');
-		let toolbar = $('.toolbar');
+
+		// if(window_width <= 1400){
+		// 	menu.add(toolbar).appendTo(navbarDrop);
+		// } else {
+		// 	menu.add(toolbar).appendTo(navbar);
+		// }
 
 		if(window_width <= 1400){
-			menu.add(toolbar).appendTo(navbarDrop);
+			menu.appendTo(navbarDrop);
 		} else {
-			menu.add(toolbar).appendTo(navbar);
+			menu.appendTo(navbar);
 		}
+
+		let navbarMobile = $('.navbar-mobile');
+		let navbarMobileLeft = $('.navbar-mobile-left');
+		let navbarMobileRight = $('.navbar-mobile-right');
+		let burger = $('.burger');
+		let toolbar = $('.toolbar');
+		let search = $('.search');
+
+		if(window_width <= 767){
+			navbarMobile.addClass('active');
+			search.appendTo(navbarMobileLeft);
+			burger.appendTo(navbarMobileRight);
+
+		} else {
+			navbarMobile.removeClass('active');
+			search.prependTo(toolbar);
+			burger.appendTo(navbar);
+		}
+
+		let filterButton = $('.filter-button');
+		let headerFilter = $('.header-filter');
+
+		filterButton.on('click', function(){
+			headerFilter.toggleClass('active');
+			$('body').toggleClass('lock');
+		});
 	});
 })(jQuery);
